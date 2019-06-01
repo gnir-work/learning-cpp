770287956887003561f5ea1aa407b261fde88c07
Nir Geller <gnir.work@gmail.com>
2019-06-01 15:53:05 +0300

    Separated the code into two files

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
-int getValueFromUser(const std::string& userPrompt);
-void printNumberTimesTwo(int number);
+int getValueFromUser(const std::string &userPrompt);
+
+int add(int x, int y);
 
 int main() {
     int userValue = getValueFromUser("Please enter a value");
-    printNumberTimesTwo(userValue);
+    int secondUserValue = getValueFromUser("Please enter a second value");
+    std::cout << "output: " << add(userValue, secondUserValue);
 }
 
-int getValueFromUser(const std::string& userPrompt) {
+int getValueFromUser(const std::string &userPrompt) {
     int userInput{0};
-    std::cout << userPrompt;
+    std::cout << userPrompt << ": ";
     std::cin >> userInput;
     return userInput;
 }
 
-void printNumberTimesTwo(int number) {
-    std::cout << number * 2;
-}
---
+++ HelloWorld/math.cpp
@@ -0,0 +1,7 @@
+//
+// Created by gnir on 6/1/19.
+//
+
+int add(int x, int y) {
+    return x + y;
+}
