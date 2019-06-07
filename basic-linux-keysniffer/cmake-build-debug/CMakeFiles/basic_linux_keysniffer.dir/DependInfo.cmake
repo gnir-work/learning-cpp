40fc7cad11bee22a9fce6fc02bb6cfd8fe50a97e
Nir Geller <gnir.work@gmail.com>
2019-06-07 18:28:07 +0300

    Forgot cmake files.

---
+++ basic-linux-keysniffer/cmake-build-debug/CMakeFiles/basic_linux_keysniffer.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gnir/Code/learning-cpp/basic-linux-keysniffer/io/KeyboardEventReader.cpp" "/home/gnir/Code/learning-cpp/basic-linux-keysniffer/cmake-build-debug/CMakeFiles/basic_linux_keysniffer.dir/io/KeyboardEventReader.cpp.o"
+  "/home/gnir/Code/learning-cpp/basic-linux-keysniffer/io/consts.cpp" "/home/gnir/Code/learning-cpp/basic-linux-keysniffer/cmake-build-debug/CMakeFiles/basic_linux_keysniffer.dir/io/consts.cpp.o"
+  "/home/gnir/Code/learning-cpp/basic-linux-keysniffer/main.cpp" "/home/gnir/Code/learning-cpp/basic-linux-keysniffer/cmake-build-debug/CMakeFiles/basic_linux_keysniffer.dir/main.cpp.o"
+  "/home/gnir/Code/learning-cpp/basic-linux-keysniffer/utils/keyboardEventUtils.cpp" "/home/gnir/Code/learning-cpp/basic-linux-keysniffer/cmake-build-debug/CMakeFiles/basic_linux_keysniffer.dir/utils/keyboardEventUtils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ basic-linux-keysniffer/cmake-build-debug/CMakeFiles/basic_linux_keysniffer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_linux_keysniffer.dir/main.cpp.o"
+  "CMakeFiles/basic_linux_keysniffer.dir/io/KeyboardEventReader.cpp.o"
+  "CMakeFiles/basic_linux_keysniffer.dir/io/consts.cpp.o"
+  "CMakeFiles/basic_linux_keysniffer.dir/utils/keyboardEventUtils.cpp.o"
+  "basic_linux_keysniffer.pdb"
+  "basic_linux_keysniffer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_linux_keysniffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
